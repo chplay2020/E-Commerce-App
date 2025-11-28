727bf2282600dfb581e6af0b54123c0e09c66c3d
Thành Trần <ratosan19@gmail.com>
2025-11-28 07:59:53 +0700

    view-order-ad

--- client/src/components/admin-view/orders.jsx
+++ client/src/components/admin-view/orders.jsx
@@ -1,11 +1,63 @@
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { useEffect, useState } from "react";
+import { Button } from "../ui/button";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow, } from "../ui/table";
+import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
+import AdminOrderDetailsView from "./order-details";
 
 
-function AdminOrders() {
+function AdminOrdersView() {
+
+    const [openDetailsDialog, setOpenDetailsDialog] = useState(false);
+
     return (
-        <div>
-            Admin Orders View
-        </div>
+        <Card>
+            <CardHeader>
+                <CardTitle>
+                    All Orders
+                </CardTitle>
+            </CardHeader>
+
+            <CardContent>
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>Order ID</TableHead>
+                            <TableHead>Order Date</TableHead>
+                            <TableHead>Order Status</TableHead>
+                            <TableHead>Order Price</TableHead>
+                            <TableHead>
+                                <span className="sr-only">Details</span>
+                            </TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>123456</TableCell>
+                            <TableCell>27/06/2024</TableCell>
+                            <TableCell>In Process</TableCell>
+                            <TableCell>$1000</TableCell>
+                            <TableCell>
+                                <Dialog
+                                    open={openDetailsDialog}
+                                    onOpenChange={setOpenDetailsDialog}
+                                >
+                                    <DialogTrigger asChild>
+                                        <Button onClick={() => setOpenDetailsDialog(true)}>
+                                            View Details
+                                        </Button>
+                                    </DialogTrigger>
+                                    <DialogContent className="sm:max-w-[600px]">
+                                        <AdminOrderDetailsView />
+                                    </DialogContent>
+                                </Dialog>
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </CardContent>
+        </Card>
     )
 }
 
-export default AdminOrders;
+export default AdminOrdersView;
