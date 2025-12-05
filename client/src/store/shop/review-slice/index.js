c7c30b0bdd963e44c38b1ce44631727950818616
Thành Trần <ratosan19@gmail.com>
2025-12-05 22:26:18 +0700

    Add review Redux slice

---
+++ client/src/store/shop/review-slice/index.js
@@ -0,0 +1,56 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+
+
+const initialState = {
+    isLoading: false,
+    error: null,
+    reviews: []
+}
+
+export const addReview = createAsyncThunk(
+    "/order/addReview",
+    async (data) => {
+        const response = await axios.post(
+            `http://localhost:5000/api/shop/review/add`,
+            { data }
+        );
+
+        return response.data;
+    }
+);
+
+export const getReviews = createAsyncThunk(
+    "/order/getReviews",
+    async (id) => {
+        const response = await axios.get(
+            `http://localhost:5000/api/shop/review/${id}`
+        );
+
+        return response.data;
+    }
+);
+
+const reviewSlice = createSlice({
+    name: 'reviewSlice',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(getReviews.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(getReviews.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.reviews = action.payload.data;
+            })
+            .addCase(getReviews.rejected, (state) => {
+                state.isLoading = false;
+                state.reviews = [];
+            })
+    }
+})
+
+//export const { } = reviewSlice.actions;
+
+export default reviewSlice.reducer;
