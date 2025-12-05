9ca4e1905b6ede6e85ee7660c290c547660230a7
Thành Trần <ratosan19@gmail.com>
2025-12-05 22:26:58 +0700

    Update server with new routes for dashboard, users, review and search

--- server/server.js
+++ server/server.js
@@ -6,11 +6,15 @@ const cors = require('cors')
 const authRouter = require('./routes/auth/auth-routes.js')
 const adminProductsRouter = require('./routes/admin/products-routes.js')
 const adminOrdersRouter = require('./routes/admin/orders-routes.js')
+const adminDashboardRouter = require('./routes/admin/dashboard-routes.js')
+const adminUsersRouter = require('./routes/admin/users-routes.js')
+
 const shopProductsRouter = require('./routes/shop/product-routes.js')
 const shopCartRouter = require('./routes/shop/cart-routes.js')
 const shopAddressRouter = require('./routes/shop/address-routes.js')
 const shopOrderRouter = require('./routes/shop/order-routes.js')
-
+const shopSearchRouter = require('./routes/shop/search-routes.js')
+const shopReviewRouter = require('./routes/shop/review-routes.js')
 
 // create a database connection
 // can create a separate file for this and import/use that file here
@@ -47,10 +51,15 @@ app.use(express.json())
 app.use('/api/auth', authRouter)
 app.use('/api/admin/products', adminProductsRouter)
 app.use('/api/admin/orders', adminOrdersRouter)
+app.use('/api/admin/dashboard', adminDashboardRouter)
+app.use('/api/admin/users', adminUsersRouter)
+
 app.use('/api/shop/products', shopProductsRouter)
 app.use('/api/shop/cart', shopCartRouter)
 app.use('/api/shop/address', shopAddressRouter)
 app.use('/api/shop/order', shopOrderRouter)
+app.use('/api/shop/search', shopSearchRouter)
+app.use('/api/shop/review', shopReviewRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
